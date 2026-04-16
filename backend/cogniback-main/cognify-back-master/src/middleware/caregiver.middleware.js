9f59f2993ce7bcb662acbad01cc10628758f5329
aryansri-coc <aryansri.coc@gmail.com>
2026-04-16 14:02:01 +0530

    Delete backend/cogniback-main directory

--- backend/cogniback-main/cognify-back-master/src/middleware/caregiver.middleware.js
+++
@@ -1,6 +0,0 @@
-exports.verifyCaregiver = (req, res, next) => {
-  if (req.user.role !== "CAREGIVER") {
-    return res.status(403).json({ message: "Access denied. Caregivers only." });
-  }
-  next();
-};
